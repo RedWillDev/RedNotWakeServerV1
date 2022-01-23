61254adad2bf4eead5cda2785e47a7462e111d10
RedWillDev <redouane.azz76@gmail.com>
2022-01-23 13:54:10 +0100

    Add tcp socket

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
 import socket
 import os
 
+def UDP():
+    UDP_IP = "192.168.1.20"
+    UDP_PORT = 80
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((UDP_IP, UDP_PORT))
+    while True:
+        data, addr = sock.recvfrom(1024)
+        print("received message: %s" % data)
 
-UDP_IP = "192.168.1.20"
-UDP_PORT = 28075
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
-while True:
-    data, addr = sock.recvfrom(1024)
-    print("received message: %s" % data)
+        if data == b'TurnOff':
+            os.system("shutdown /s /t 30")
+        elif data == b'TurnON':
+            print("test")
 
-    if data == b'TurnOff':
-        os.system("shutdown /s /t 30")
-    elif data == b'TurnON':
-        os.system("shutdown /a")
+def TCP():
+    UDP_IP = "192.168.1.20"
+    UDP_PORT = 80
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.bind((UDP_IP, UDP_PORT))
+    while True:
+        data, addr = sock.recvfrom(1024)
+        print("received message: %s" % data)
+
+        if data == b'TurnOff':
+            os.system("shutdown /s /t 30")
+        elif data == b'TurnON':
+            print("test")
+
+
+
+var = input("? : ")
+
+if var == "UDP" or var == "udp":
+    UDP()
+elif var == "TCP" or var == "tcp":
+    TCP()
+
+
